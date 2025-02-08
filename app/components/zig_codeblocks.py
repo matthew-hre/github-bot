1a79be7e98a86ea716d4668271eccdd50799b9ec
trag1c <trag1cdev@yahoo.com>
2025-02-08 03:54:46 +0100

    feat: react to `.zig` files

--- app/components/zig_codeblocks.py
+++ app/components/zig_codeblocks.py
@@ -1,7 +1,12 @@
+from io import BytesIO
+
 import discord
-from zig_codeblocks import extract_codeblocks, process_markdown
+from zig_codeblocks import extract_codeblocks, highlight_zig_code, process_markdown
 
 from app.utils import is_dm, is_mod
+
+MAX_CONTENT = 51_200  # 50 KiB
+MAX_ZIG_FILE_SIZE = 8_388_608  # 8 MiB
 
 
 class DismissCode(discord.ui.View):
@@ -29,10 +34,30 @@ class DismissCode(discord.ui.View):
 
 
 async def check_for_zig_code(message: discord.Message) -> None:
+    attachments = [
+        discord.File(
+            BytesIO(
+                highlight_zig_code((await att.read()).decode()[:MAX_CONTENT]).encode()
+            ),
+            att.filename + ".ansi",
+        )
+        for att in message.attachments
+        if att.filename.endswith(".zig") and att.size <= MAX_ZIG_FILE_SIZE
+    ]
     codeblocks = list(extract_codeblocks(message.content))
     if codeblocks and any(block.lang == "zig" for block in codeblocks):
         zig_code = process_markdown(message.content, only_code=True)
         if len(zig_code) <= 2000:
             await message.reply(
-                zig_code, view=DismissCode(message), mention_author=False
+                zig_code,
+                view=DismissCode(message),
+                files=attachments,
+                mention_author=False,
             )
+    elif attachments:
+        await message.reply(
+            'Click "View whole file" to see the highlighting.',
+            view=DismissCode(message),
+            files=attachments,
+            mention_author=False,
+        )
