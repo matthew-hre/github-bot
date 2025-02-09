7d91775c7c43af4a035b2b487671bfa8a314ff63
trag1c <trag1cdev@yahoo.com>
2025-02-09 20:48:20 +0100

    feat: disable comment highlighting w/ zig-codeblocks@0.2.0

--- app/components/zig_codeblocks.py
+++ app/components/zig_codeblocks.py
@@ -1,12 +1,19 @@
 from io import BytesIO
 
 import discord
-from zig_codeblocks import extract_codeblocks, highlight_zig_code, process_markdown
+from zig_codeblocks import (
+    DEFAULT_THEME,
+    extract_codeblocks,
+    highlight_zig_code,
+    process_markdown,
+)
 
 from app.utils import is_dm, is_mod
 
 MAX_CONTENT = 51_200  # 50 KiB
 MAX_ZIG_FILE_SIZE = 8_388_608  # 8 MiB
+THEME = DEFAULT_THEME.copy()
+THEME.comments = None
 
 
 class DismissCode(discord.ui.View):
@@ -36,7 +43,9 @@ class DismissCode(discord.ui.View):
 async def check_for_zig_code(message: discord.Message) -> None:
     attachments = [
         discord.File(
-            BytesIO(highlight_zig_code((await att.read())[:MAX_CONTENT]).encode()),
+            BytesIO(
+                highlight_zig_code((await att.read())[:MAX_CONTENT], THEME).encode()
+            ),
             att.filename + ".ansi",
         )
         for att in message.attachments
@@ -44,7 +53,7 @@ async def check_for_zig_code(message: discord.Message) -> None:
     ]
     codeblocks = list(extract_codeblocks(message.content))
     if codeblocks and any(block.lang == "zig" for block in codeblocks):
-        zig_code = process_markdown(message.content, only_code=True)
+        zig_code = process_markdown(message.content, THEME, only_code=True)
         if len(zig_code) <= 2000:
             await message.reply(
                 zig_code,
