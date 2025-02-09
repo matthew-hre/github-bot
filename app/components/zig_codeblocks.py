1b6782f52b24e74470070cf511be7097d2c3e77a
trag1c <trag1cdev@yahoo.com>
2025-02-10 00:29:57 +0100

    feat(zig-codeblocks): allow freezing messages

--- app/components/zig_codeblocks.py
+++ app/components/zig_codeblocks.py
@@ -17,9 +17,10 @@ THEME = DEFAULT_THEME.copy()
 THEME.comments = None
 
 message_to_codeblocks: dict[discord.Message, discord.Message] = {}
+frozen_messages = set[discord.Message]()
 
 
-class DismissCode(discord.ui.View):
+class ZigCodeblockActions(discord.ui.View):
     def __init__(self, message: discord.Message) -> None:
         super().__init__()
         self._message = message
@@ -41,6 +42,29 @@ class DismissCode(discord.ui.View):
         await interaction.response.send_message(
             "You can't dismiss this message.", ephemeral=True
         )
+
+    @discord.ui.button(
+        label="Freeze",
+        emoji="❄️",
+        style=discord.ButtonStyle.gray,
+    )
+    async def freeze(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ) -> None:
+        assert not is_dm(interaction.user)
+        if interaction.user.id == self._message.author.id or is_mod(interaction.user):
+            frozen_messages.add(self._message)
+            button.disabled = True
+            await interaction.response.edit_message(view=self)
+            await interaction.followup.send(
+                "Message frozen. I will no longer react to"
+                " what happens to your original message.",
+                ephemeral=True,
+            )
+        else:
+            await interaction.response.send_message(
+                "You can't freeze this message.", ephemeral=True
+            )
 
 
 async def _prepare_reply(
@@ -77,7 +101,7 @@ async def check_for_zig_code(message: discord.Message) -> None:
         return
     reply = await message.reply(
         content,
-        view=DismissCode(message),
+        view=ZigCodeblockActions(message),
         files=files,
         mention_author=False,
     )
@@ -97,14 +121,14 @@ async def zig_codeblock_edit_handler(
         return
 
     if (reply := message_to_codeblocks.get(before)) is None:
-        if not old_content:
+        if not (old_content or before in frozen_messages):
             # There was no code before, so treat this as a new message.
             # Note: No new attachments can appear, their count can only decrease.
             await check_for_zig_code(after)
         # The message was removed from the M2C map at some point
         return
 
-    if not (new_content or new_files):
+    if not (new_content or new_files or before in frozen_messages):
         # All code was edited out
         del message_to_codeblocks[before]
         await reply.delete()
@@ -114,12 +138,16 @@ async def zig_codeblock_edit_handler(
     # stop reacting to it and remove its M2C entry.
     last_updated = dt.datetime.now(tz=dt.UTC) - (reply.edited_at or reply.created_at)
     if last_updated > dt.timedelta(hours=24):
+        frozen_messages.discard(before)
         del message_to_codeblocks[before]
+        return
+
+    if before in frozen_messages:
         return
 
     await reply.edit(
         content=new_content,
-        view=DismissCode(after),
+        view=ZigCodeblockActions(after),
         attachments=new_files,
         allowed_mentions=discord.AllowedMentions.none(),
     )
@@ -132,11 +160,15 @@ def _unlink_original_message(message: discord.Message) -> None:
         None,
     )
     if original_message is not None:
+        frozen_messages.discard(original_message)
         del message_to_codeblocks[original_message]
 
 
 async def zig_codeblock_delete_handler(message: discord.Message) -> None:
     if message.author.bot:
         _unlink_original_message(message)
-    if (reply := message_to_codeblocks.get(message)) is not None:
+    if not (
+        (reply := message_to_codeblocks.get(message)) is None
+        or message in frozen_messages
+    ):
         await reply.delete()
