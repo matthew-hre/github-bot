2e182b2c6a9177d2b3946e4cf9ebdbb1d80d9e5e
trag1c <trag1cdev@yahoo.com>
2025-03-03 14:04:33 +0100

    feat: support issue/PR comments

--- app/components/entity_mentions/comments.py
+++ app/components/entity_mentions/comments.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
+import asyncio
 import re
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 import discord
 
+from .cache import entity_cache
 from .discussions import get_discussion_comment
 from .integration import DeleteMention
-from .models import Comment, EntityGist
+from .models import Comment, CommentAuthor, EntityGist
+from app.setup import gh
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator
@@ -17,12 +20,34 @@ COMMENT_PATTERN = re.compile(
 )
 
 
+async def _get_issue_comment(entity_gist: EntityGist, comment_id: int) -> Comment:
+    owner, repo, _ = entity_gist
+    comment_resp, entity = await asyncio.gather(
+        gh.rest.issues.async_get_comment(owner, repo, comment_id),
+        entity_cache.get(entity_gist),
+    )
+    author = (comment := comment_resp.parsed_data).user
+    assert author is not None
+    return Comment(
+        author=CommentAuthor(
+            name=author.login, url=author.html_url, icon_url=author.avatar_url
+        ),
+        body=cast(str, comment.body),
+        entity_title=entity.title,
+        entity_gist=entity_gist,
+        created_at=comment.created_at,
+        html_url=comment.html_url,
+    )
+
+
 async def get_comments(content: str) -> AsyncIterator[Comment]:
     for match in COMMENT_PATTERN.finditer(content):
         owner, repo, _kind, number, event, event_no = map(str, match.groups())
         entity_gist = EntityGist(owner, repo, int(number))
         if event.startswith("discussion"):
             yield await get_discussion_comment(int(event_no), entity_gist)
+        elif event.startswith("issuecomment"):
+            yield await _get_issue_comment(entity_gist, int(event_no))
 
 
 def comment_to_embed(comment: Comment) -> discord.Embed:
