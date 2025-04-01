be0cd11974b157146dd84fef3ae891a084e28dcd
Kat <65649991+00-kat@users.noreply.github.com>
2025-04-01 17:57:57 +1100

    Add a “Show Transcript” button to XKCD mention replies.

--- app/components/xkcd_mentions.py
+++ app/components/xkcd_mentions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 import datetime as dt
 import re
@@ -15,6 +17,7 @@ from app.utils import (
     remove_view_after_timeout,
 )
 
+SECONDS_IN_HOUR = 3600
 XKCD_REGEX = re.compile(r"\bxkcd#(\d+)", re.IGNORECASE)
 
 
@@ -24,10 +27,11 @@ class XKCD(BaseModel):
     year: int
     img: str
     title: str
+    transcript: str
     alt: str
 
 
-class XKCDMentionCache(TTRCache[int, discord.Embed]):
+class XKCDMentionCache(TTRCache[int, tuple[discord.Embed, str | None]]):
     async def fetch(self, key: int) -> None:
         url = f"https://xkcd.com/{key}"
         async with httpx.AsyncClient() as client:
@@ -38,7 +42,8 @@ class XKCDMentionCache(TTRCache[int, discord.Embed]):
                 if resp.status_code == 404
                 else f"Unable to fetch XKCD #{key}."
             )
-            self[key] = discord.Embed(color=discord.Color.red()).set_footer(text=error)
+            embed = discord.Embed(color=discord.Color.red()).set_footer(text=error)
+            self[key] = (embed, None)
             return
 
         xkcd = XKCD(**resp.json())
@@ -51,7 +56,8 @@ class XKCDMentionCache(TTRCache[int, discord.Embed]):
         self[key] = (
             discord.Embed(title=xkcd.title, url=url)
             .set_image(url=xkcd.img)
-            .set_footer(text=f"{xkcd.alt} • {date}")
+            .set_footer(text=f"{xkcd.alt} • {date}"),
+            xkcd.transcript,
         )
 
 
@@ -59,10 +65,72 @@ xkcd_mention_cache = XKCDMentionCache(hours=12)
 xkcd_mention_linker = MessageLinker()
 
 
-class DeleteXKCDMention(DeleteMessage):
+class TranscriptPicker(discord.ui.View):
+    select: discord.ui.Select[TranscriptPicker]
+
+    def __init__(self, transcripts: dict[int, tuple[str, str]]) -> None:
+        super().__init__()
+        self.transcripts = transcripts
+        self._add_selection_box()
+
+    def _add_selection_box(self) -> None:
+        self.select = discord.ui.Select(
+            placeholder="Select XKCD comics", max_values=len(self.transcripts)
+        )
+        for number, (name, _) in self.transcripts.items():
+            self.select.add_option(label=name, value=str(number))
+        self.select.callback = self._callback
+        self.add_item(self.select)
+
+    async def _callback(self, interaction: discord.Interaction) -> None:
+        embeds: list[discord.Embed] = []
+        for comic in self.select.values:
+            name, transcript = self.transcripts[int(comic)]
+            embeds.append(self.format_transcript(comic, name, transcript))
+        await interaction.response.edit_message(embeds=embeds, view=None)
+
+    @staticmethod
+    def format_transcript(
+        comic: int | str, name: str, transcript: str
+    ) -> discord.Embed:
+        return discord.Embed(
+            title=name, url=f"https://xkcd.com/{comic}", description=transcript
+        )
+
+
+class XKCDMentionActions(DeleteMessage):
     linker = xkcd_mention_linker
     action_singular = "linked this XKCD comic"
     action_plural = "linked these XKCD comics"
+
+    async def _get_transcripts(
+        self, embeds: list[discord.Embed]
+    ) -> dict[int, tuple[str, str]]:
+        transcripts: dict[int, tuple[str, str]] = {}
+        for embed in embeds:
+            if not (embed.url and embed.title):
+                continue
+            number = int(embed.url.rstrip("/").rpartition("/")[2])
+            _, transcript = await xkcd_mention_cache.get(number)
+            if transcript is None:
+                continue
+            transcripts[number] = (embed.title, transcript)
+        return transcripts
+
+    @discord.ui.button(
+        label="Show Transcript", emoji="📜", style=discord.ButtonStyle.gray
+    )
+    async def show_transcript(
+        self,
+        interaction: discord.Interaction,
+        _button: discord.ui.Button[XKCDMentionActions],
+    ) -> None:
+        reply, *_ = self.linker.get(self.message)
+        transcripts = await self._get_transcripts(reply.embeds)
+        await interaction.response.send_message(
+            view=TranscriptPicker(transcripts),
+            ephemeral=True,
+        )
 
 
 async def xkcd_mention_message(
@@ -78,7 +146,12 @@ async def xkcd_mention_message(
         ]
         matches = matches[:9]
     embeds = [
-        *await asyncio.gather(*(xkcd_mention_cache.get(int(m)) for m in matches)),
+        *(
+            embed
+            for embed, _ in await asyncio.gather(
+                *(xkcd_mention_cache.get(int(m)) for m in matches)
+            )
+        ),
         *embeds,
     ]
     return embeds, len(embeds)
@@ -92,12 +165,12 @@ async def handle_xkcd_mentions(message: discord.Message) -> None:
         return
     try:
         sent_message = await message.reply(
-            embeds=embeds, mention_author=False, view=DeleteXKCDMention(message, count)
+            embeds=embeds, mention_author=False, view=XKCDMentionActions(message, count)
         )
     except discord.HTTPException:
         return
     xkcd_mention_linker.link(message, sent_message)
-    await remove_view_after_timeout(sent_message)
+    await remove_view_after_timeout(sent_message, SECONDS_IN_HOUR)
 
 
 xkcd_mention_delete_hook = create_delete_hook(linker=xkcd_mention_linker)
@@ -106,6 +179,6 @@ xkcd_mention_edit_hook = create_edit_hook(
     linker=xkcd_mention_linker,
     message_processor=xkcd_mention_message,
     interactor=handle_xkcd_mentions,
-    view_type=DeleteXKCDMention,
+    view_type=XKCDMentionActions,
     embed_mode=True,
 )
