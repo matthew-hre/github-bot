5e1470ac3a182e4b66bef442d30e30ebee9034e5
Kat <65649991+00-kat@users.noreply.github.com>
2025-04-07 09:25:46 +1000

    Avoid storing all messages when getting them for locking a post.
    Co-authored-by: trag1c <dev@jakubr.me>

--- app/components/lock_old_posts.py
+++ app/components/lock_old_posts.py
@@ -5,7 +5,7 @@ import datetime as dt
 import discord
 
 from app import config
-from app.utils import dynamic_timestamp, post_is_solved
+from app.utils import aenumerate, dynamic_timestamp, post_is_solved
 
 
 async def check_for_old_posts(message: discord.Message) -> None:
@@ -21,20 +21,19 @@ async def check_for_old_posts(message: discord.Message) -> None:
     ):
         return
 
-    try:
-        # Ignore messages less than a minute old for the same person may have
-        # sent multiple in quick succession, resulting in their own message
-        # being considered as activity.
-        last = await _get_message(post, 1, before=now - dt.timedelta(minutes=1))
-    except IndexError:
-        # There were no messages older than one minute. Assuming the threshold
-        # below stays at a reasonably large limit, this either means that every
-        # message was deleted, or that there was no response and the original
-        # poster deleted the starter message. It's very unlikely for the post
-        # to no longer be relevant; nor is it likely that anyone would even
-        # message in such a thread. Thus, silently exit if this is the case.
-        return
-    if last.created_at > now - dt.timedelta(days=30):
+    # Ignore messages less than a minute old for the same person may have
+    # sent multiple in quick succession, resulting in their own message
+    # being considered as activity.
+    last = await _get_message(post, 1, before=now - dt.timedelta(minutes=1))
+    # Don't lock the post if it isn't old enough.
+    if last is None or last.created_at > now - dt.timedelta(days=30):
+        # If last was None, there were no messages older than one minute.
+        # Assuming the threshold above stays at a reasonably large limit, this
+        # either means that every message was deleted, or that there was no
+        # response and the original poster deleted the starter message. It's
+        # very unlikely for the post to no longer be relevant; nor is it likely
+        # that anyone would even message in such a thread. Thus, silently exit
+        # if this is the case.
         return
 
     try:
@@ -57,8 +56,6 @@ async def _get_message(
     *,
     before: discord.abc.SnowflakeTime | None = None,
     around: discord.abc.SnowflakeTime | None = None,
-) -> discord.Message:
-    return [
-        message
-        async for message in thread.history(limit=n + 1, before=before, around=around)
-    ][n]
+) -> discord.Message | None:
+    messages = thread.history(limit=n + 1, before=before, around=around)
+    return await anext((m async for i, m in aenumerate(messages) if i == n), None)
