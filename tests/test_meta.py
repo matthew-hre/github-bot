0924cbbbb27cfa7e3b65f235a69e186c1afac2a6
Kat <65649991+00-kat@users.noreply.github.com>
2025-06-24 14:19:56 +1000

    Use \uXXXX escape sequences instead of raw UTF-8 bytes for VS16 check.

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 import pytest
 
-VS16 = b"\xef\xb8\x8f"
-VS16_SUPPRESS_COMMENT = b"# test: allow-vs16"
+VS16 = "\ufe0f"
+VS16_SUPPRESS_COMMENT = "# test: allow-vs16"
 
 
 def check_dir_for_vs16(path: Path) -> None:
     error_locations: list[tuple[Path, int]] = []
     for file in path.rglob("*.py"):
-        for line_no, line in enumerate(file.read_bytes().splitlines(), 1):
+        for line_no, line in enumerate(file.read_text().splitlines(), 1):
             if VS16 in line and VS16_SUPPRESS_COMMENT not in line:
                 error_locations.append((file, line_no))
 
@@ -28,12 +28,12 @@ def test_self_unwanted_vs16(folder: str) -> None:
 
 def test_dummy_unwanted_vs16(tmp_path: Path) -> None:
     (tmp_path / "foo").mkdir()
-    (tmp_path / "foo" / "bar.py").write_bytes(VS16)
-    (tmp_path / "foo" / "baz.py").write_bytes(b"\xe2\x9d\xa4" + VS16)
+    (tmp_path / "foo" / "bar.py").write_text(VS16)
+    (tmp_path / "foo" / "baz.py").write_text("\u2764" + VS16)
 
     (tmp_path / "qux").mkdir()
-    (tmp_path / "qux" / "foo.py").write_bytes(b"\xe2\x9d\xa4\xef")
-    (tmp_path / "qux" / "bar.txt").write_bytes(VS16)
+    (tmp_path / "qux" / "foo.py").write_text("\u2764\ufffd")
+    (tmp_path / "qux" / "bar.txt").write_text(VS16)
 
     check_dir_for_vs16(tmp_path / "qux")
 
