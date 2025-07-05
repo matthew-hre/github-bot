db058faa8ded4fae7aab41dde98eb646ca3515b2
trag1c <dev@jakubr.me>
2025-07-05 16:37:05 +0200

    extract common checks for freeze/delete actions

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -88,23 +88,29 @@ class ItemActions(discord.ui.View):
         self.message = message
         self.item_count = item_count
 
+    async def _reject_early(
+        self, interaction: discord.Interaction, action: str
+    ) -> bool:
+        assert not is_dm(interaction.user)
+        if interaction.user.id == self.message.author.id or is_mod(interaction.user):
+            return False
+        await interaction.response.send_message(
+            "Only the person who "
+            + (self.action_singular if self.item_count == 1 else self.action_plural)
+            + f" can {action} this message.",
+            ephemeral=True,
+        )
+        return True
+
     @discord.ui.button(label="Delete", emoji="❌")
     async def delete(
         self, interaction: discord.Interaction, _: discord.ui.Button[Self]
     ) -> None:
-        assert not is_dm(interaction.user)
-        if interaction.user.id == self.message.author.id or is_mod(interaction.user):
-            assert interaction.message
-            await interaction.message.delete()
-            self.linker.unlink_from_reply(interaction.message)
+        if await self._reject_early(interaction, "remove"):
             return
-
-        await interaction.response.send_message(
-            "Only the person who "
-            + (self.action_singular if self.item_count == 1 else self.action_plural)
-            + " can remove this message.",
-            ephemeral=True,
-        )
+        assert interaction.message
+        await interaction.message.delete()
+        self.linker.unlink_from_reply(interaction.message)
 
     @discord.ui.button(label="Freeze", emoji="❄️")  # test: allow-vs16
     async def freeze(
@@ -112,22 +118,14 @@ class ItemActions(discord.ui.View):
         interaction: discord.Interaction,
         button: discord.ui.Button[Self],
     ) -> None:
-        assert not is_dm(interaction.user)
-        if interaction.user.id == self.message.author.id or is_mod(interaction.user):
-            self.linker.freeze(self.message)
-            button.disabled = True
-            await interaction.response.edit_message(view=self)
-            await interaction.followup.send(
-                "Message frozen. I will no longer react to"
-                " what happens to your original message.",
-                ephemeral=True,
-            )
+        if await self._reject_early(interaction, "freeze"):
             return
-
-        await interaction.response.send_message(
-            "Only the person who "
-            + (self.action_singular if self.item_count == 1 else self.action_plural)
-            + " can freeze this message.",
+        self.linker.freeze(self.message)
+        button.disabled = True
+        await interaction.response.edit_message(view=self)
+        await interaction.followup.send(
+            "Message frozen. I will no longer react to"
+            " what happens to your original message.",
             ephemeral=True,
         )
 
