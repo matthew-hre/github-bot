1eea0dc41de6d687b94e9519086bea45694b5955
trag1c <dev@jakubr.me>
2025-08-12 12:40:25 +0200

    bump deps

--- app/components/__init__.py
+++ app/components/__init__.py
@@ -1,4 +1,3 @@
-# ruff: noqa: PLC0414
 # Some modules are not imported by any other module so we're loading them manually:
 from . import accept_invite as accept_invite
 from . import close_help_post as close_help_post
