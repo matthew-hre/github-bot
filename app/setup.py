b22688b1016e073b4741f29375572959a4dd7909
Kat <65649991+00-kat@users.noreply.github.com>
2025-08-25 15:01:06 +1000

    Forward logs from the standard logging module to Loguru.
    As evident by the diff in uv.lock a few commits ago, none of our
    dependencies uses Loguru for logging, but rather uses the `logging`
    module in the standard library. So we shall make the standard logging
    module divert its logs to Loguru instead.

--- app/setup.py
+++ app/setup.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import inspect
+import logging
 import os
 import sys
 from functools import cached_property
@@ -82,6 +84,36 @@ bot = commands.Bot(
 )
 
 gh = GitHub(config.github_token)
+
+
+# Both discord.py and httpx use the standard logging module; redirect them to Loguru.
+# This code snippet is taken straight from Loguru's README:
+# https://github.com/Delgan/loguru/tree/0.7.3#entirely-compatible-with-standard-logging
+class _InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        try:
+            level: str | int = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame:
+            filename = frame.f_code.co_filename
+            is_logging = filename == logging.__file__
+            is_frozen = "importlib" in filename and "_bootstrap" in filename
+            if depth > 0 and not (is_logging or is_frozen):
+                break
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+logging.basicConfig(handlers=[_InterceptHandler()], level=0, force=True)
 
 logger.remove()
 logger.add(
