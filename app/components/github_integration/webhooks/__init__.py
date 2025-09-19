732e2f30a11131ca0c6b5fe92f2caa2889fdfa9c
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2025-09-19 19:09:10 -0400

    chore: don't export unused objects

--- app/components/github_integration/webhooks/__init__.py
+++ app/components/github_integration/webhooks/__init__.py
@@ -1,6 +1,3 @@
 from .core import client as monalisten_client
-from .discussions import Discussions
-from .issues import Issues
-from .prs import PRs
 
-__all__ = ("Discussions", "Issues", "PRs", "monalisten_client")
+__all__ = ("monalisten_client",)
