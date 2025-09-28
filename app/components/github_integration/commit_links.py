69670378bb8309bf10d014c29aa09d00b790e7a5
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2025-09-28 08:37:02 -0400

    chore: use TaskGroup where logical

--- app/components/github_integration/commit_links.py
+++ app/components/github_integration/commit_links.py
@@ -140,10 +140,9 @@ class CommitLinks(commands.Cog):
             view=CommitActions(message, output.item_count),
         )
         self.linker.link(message, reply)
-        await asyncio.gather(
-            suppress_embeds_after_delay(message),
-            remove_view_after_delay(reply),
-        )
+        async with asyncio.TaskGroup() as group:
+            group.create_task(suppress_embeds_after_delay(message))
+            group.create_task(remove_view_after_delay(reply))
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: dc.Message) -> None:
